19779461980d6a567fb00299d83a3f198874eaea
jsonwamuyu <jysonmuchiri@gmail.com>
2025-05-07 14:19:14 +0300

    Data structures

---
+++ milestone-1/practice/data_structures.py
@@ -0,0 +1,18 @@
+"""
+List, Tuple, Set, Dictionary, String
+"""
+
+print("This is awesome")
+
+my_list = ["one", "two", "three", "four", 456, ['Jane', "Mike", 78]]
+
+print(my_list[0]) # Print first item -> one
+print(my_list[-1]) # Print the last item
+
+print(my_list) # Print the whole list
+print(my_list[1:3]) # Slice from index 1 to 3
+
+
+print()
+
+# Tuple
--- milestone-1/practice/functions.py
+++ milestone-1/practice/functions.py
@@ -10,31 +10,55 @@ Function Design Process
     example: return length*width
 4. How will you name it?
     Use clear, descriptive names. Should indicate what the function does
-
 """
+
+# We’ll write a function to check whether a number is a prime number.
+
+
+def check_number_is_prime(num):
+    """
+    Check whether a number is prime.
+    A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+
+    :param num: Number to be checked
+
+    :return: Boolean(True is prime, False otherwise)
+    """
+    if num < 2:
+        return False
+    # for i in range(2, num - 1):
+    for i in range(2, int(num ** 0.5 ) + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+print('Test whether 3 is prime: ', check_number_is_prime(3))
+print('Test whether 8 is prime: ', check_number_is_prime(8))
+
 
 def check_number_input(num):
     """
-    Return True if number is even number(divisible by 2)
+    Return True if number is even number(divisible by 2), False otherwise
 
     Parameters:
         num(int, float): The number to be checked
 
     Returns:
-        print
+        Boolean: True if even, False otherwise
     """
 
-    if num % 2 == 0:
-        print('Even number')
-    else:
-        print("Not even number")
+    return num % 2 == 0
 
 
-check_number_input(4)
-check_number_input(1)
+print(check_number_input(4))
+print(check_number_input(1))
+
 
 def hello():
-    return 'Mike'
+    """Greet Mike."""
+    return "Mike"
 
-n = hello()
-print(n)
+
+print(hello())
